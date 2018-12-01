9188297a4cfa648fdde5932db6cac4a42eb2467a
PHUC-PC\dohongphuc <dohongphuc1997@gmail.com>
2018-12-02 02:42:51 +0700

    setup router, store
    setup router, store
    add boostrap 3

--- social-network-app/src/store/store.js
+++ social-network-app/src/store/store.js
@@ -1,18 +1,16 @@
-import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import Reducers from './reducers/index';
 import thunk from 'redux-thunk';
 
 export default () => {
     //combine all reducers
     const rootReducer = combineReducers({
-        firebase: firebaseReducer,
-        firestore: firestoreReducer,
         reducers: Reducers
       })
 
     // Create store with reducers and initial state
-    const initialState = {}
-    const store = createStore(rootReducer, initialState, createStoreWithFirebase);
+    //const initialState = {}
+    const store = createStore(rootReducer, applyMiddleware(thunk));
 
     return store;
 };
